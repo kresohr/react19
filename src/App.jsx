73229d1d49f3755577544c17bdd3dce3c9139148
kresohr <ilicic.kresimir@gmail.com>
2025-07-03 18:43:49 +0200

    Cleared App.jsx content

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import { useState } from "react";
-import "./App.css";
-
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
-
-  return (
-    <div className="card">
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(true)}>
-        {hasLiked ? "Liked" : "Like"}
-      </button>
-    </div>
-  );
-};
+import React from "react";
 
 const App = () => {
-  const [hasLiked, setHasLiked] = useState(false);
-
-  return (
-    <div className="card-container">
-      <h2>Functional Arrow Component</h2>
-      <Card title="Star Wars" />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
